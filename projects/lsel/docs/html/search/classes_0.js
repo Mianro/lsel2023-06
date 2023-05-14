2e91e1fbb507f2f53492214b746d89c9e7bf8daa
Omar Fuentes <omar.fuentes@alumnos.upm.es>
2023-05-14 22:16:18 +0200

    update on Doxygen and Gcov

--- projects/lsel/docs/html/search/classes_0.js
+++ projects/lsel/docs/html/search/classes_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['_5fcmock_5fdo_5fnothing_5fcall_5finstance_5',['_CMOCK_do_nothing_CALL_INSTANCE',['../struct__CMOCK__do__nothing__CALL__INSTANCE.html',1,'']]],
-  ['_5fcmock_5fis_5ftrue_5fcall_5finstance_6',['_CMOCK_is_true_CALL_INSTANCE',['../struct__CMOCK__is__true__CALL__INSTANCE.html',1,'']]]
+  ['_5fcmock_5fcheck_5ffunc_5fcall_5finstance_5',['_CMOCK_check_func_CALL_INSTANCE',['../struct__CMOCK__check__func__CALL__INSTANCE.html',1,'']]],
+  ['_5fcmock_5fdo_5fnothing_5fcall_5finstance_6',['_CMOCK_do_nothing_CALL_INSTANCE',['../struct__CMOCK__do__nothing__CALL__INSTANCE.html',1,'']]]
 ];
--- projects/lsel/docs/html/structmock__test__fsmInstance.js
+++ projects/lsel/docs/html/structmock__test__fsmInstance.js
@@ -1,14 +1,14 @@
 var structmock__test__fsmInstance =
 [
+    [ "check_func_CallbackBool", "structmock__test__fsmInstance.html#a55f9834a11313bc09539241d981cac21", null ],
+    [ "check_func_CallbackCalls", "structmock__test__fsmInstance.html#a0f60cab06ef577039a9d40a0ea821a77", null ],
+    [ "check_func_CallbackFunctionPointer", "structmock__test__fsmInstance.html#ada2ddd12e722d383a138850d3a3864e6", null ],
+    [ "check_func_CallInstance", "structmock__test__fsmInstance.html#a267855950d6060630adb4396ed33e9ac", null ],
+    [ "check_func_FinalReturn", "structmock__test__fsmInstance.html#a9c202907dd927480827f9066ed8a7d9b", null ],
+    [ "check_func_IgnoreBool", "structmock__test__fsmInstance.html#a289eb70054bed3564b021545dee0f32d", null ],
     [ "do_nothing_CallbackBool", "structmock__test__fsmInstance.html#ab394164f88fc1831090727277c2b2a57", null ],
     [ "do_nothing_CallbackCalls", "structmock__test__fsmInstance.html#ab234ce9ea34dbb1d62f36fd8bd626b19", null ],
     [ "do_nothing_CallbackFunctionPointer", "structmock__test__fsmInstance.html#a48cca92240193aa565dd5f59dc5c8bd4", null ],
     [ "do_nothing_CallInstance", "structmock__test__fsmInstance.html#aa70ba9cc958915adf68f1db4eac872ed", null ],
-    [ "do_nothing_IgnoreBool", "structmock__test__fsmInstance.html#a5b863b9b09d3f514b171c8a4de0a5246", null ],
-    [ "is_true_CallbackBool", "structmock__test__fsmInstance.html#a4322c2ed9d1ea1467566087f44a61299", null ],
-    [ "is_true_CallbackCalls", "structmock__test__fsmInstance.html#ac696a80054d9137186a6ecc7c43d2252", null ],
-    [ "is_true_CallbackFunctionPointer", "structmock__test__fsmInstance.html#ab84824956c0fadf224a3bc52917ffc19", null ],
-    [ "is_true_CallInstance", "structmock__test__fsmInstance.html#a8098a2dbf40fa7a7b112f966dda69f67", null ],
-    [ "is_true_FinalReturn", "structmock__test__fsmInstance.html#ad383c1e84da7cbb3a792467ca8566079", null ],
-    [ "is_true_IgnoreBool", "structmock__test__fsmInstance.html#a19eaa1dbfd7f3cd448703ac4cc43b89b", null ]
+    [ "do_nothing_IgnoreBool", "structmock__test__fsmInstance.html#a5b863b9b09d3f514b171c8a4de0a5246", null ]
 ];
